06f5be808ee0c49aac4d7fc44efe395483c78feb
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-28 22:04:10 +0530

    Updated app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+//import CenterMenu from "./components/CenterMenu";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import Download from "./components/Download";
+
 function App() {
   return (
-    <div className="App">
-      I am here
+    <div className='App text-white overflow-hidden'> {/*default colour will be white*/}
+
+      <Header/>
+      <Hero/>
+      <Experience/>
+      <Footer />
+      <Search/>
+      <Download/>
 
     </div>
 )};
