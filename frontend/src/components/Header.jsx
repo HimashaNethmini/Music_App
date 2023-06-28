5e566a8321a2ac2bdf9da6e52fe04f8c09987512
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-28 13:38:18 +0530

    Created header part - homepage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,14 +2,28 @@ import React from 'react'
 import CenterMenu from './CenterMenu';
 
 function Header() {
+    const buttonStyle = "border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]"
+
   return (
-    <div className='header'>
-        <img src={require ("../img/MuzicLogo.png")} alt="" />
+    <div className='header bg-[#081730] flex items-center
+    justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]'>
+
+        {/* App logo */}
+        <img src={require ("../img/MuzicLogo.png")} alt="" className='logo w-[42px] h-[42px]' />
+
         <CenterMenu/>
-        <div className="buttons">
-            <button>Sign up</button>
-            <button>Log in</button>
-        </div>
+
+        {/* Login & Sign up buttons */}
+        <div className="buttons flex">
+
+        <button className={`mr-[35px] hover:bg-[#232A4E] ` + buttonStyle}>
+          Log in
+        </button>
+
+        <button className={buttonStyle+` bg-[#232A4E]`}>
+            Sign up
+        </button>
+      </div>
       
     </div>
   )
